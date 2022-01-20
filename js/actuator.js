2a3152daa9edd9ea00849159ab2c2430bb5d7059
tjasz <nukorausa@hotmail.com>
2022-01-20 07:32:39 -0800

    Fill in category assignments

--- js/actuator.js
+++ js/actuator.js
@@ -1,17 +1,36 @@
 function HtmlActuator() {
-  this.lastTileContainer = document.querySelector(".last-tile");
-  this.lastTile = -1;
 }
 
 HtmlActuator.prototype.applyClasses = function (element, classes) {
   element.setAttribute("class", classes.join(" "));
 };
 
+HtmlActuator.prototype.refreshCategoryAssignments = function(grid) {
+  // refresh category display for all grid cells
+  var gridCells = document.getElementsByClassName("grid-cell");
+  for (var i = 0; i < gridCells.length; i++) {
+    var cat1 = Math.floor(grid.categoryAssignments[i] / grid.size);
+    var cat2 = grid.categoryAssignments[i] % grid.size;
+	gridCells[i].textContent = cat1+1;
+    var classes = ["grid-cell", "cat2-" + cat2];
+	this.applyClasses(gridCells[i], classes);
+  }
+  // refresh category display for the last tile cell
+  var lastTileContainer = document.querySelector(".last-tile");
+  var lastTile = grid.lastTileCategoryInfo;
+  if (lastTile >= 0) {
+    cat1 = Math.floor(lastTile / grid.size);
+    cat2 = lastTile % grid.size;
+    lastTileContainer.textContent = cat1+1;
+    classes = ["last-tile", "cat2-" + cat2];
+    this.applyClasses(lastTileContainer, classes);
+  }
+  else {
+    lastTileContainer.textContent = "";
+    this.applyClasses(lastTileContainer, "last-tile");
+  }
+};
+
 HtmlActuator.prototype.actuate = function(grid) {
-  this.lastTile = grid.lastTileCategoryInfo;
-  var cat1 = Math.floor(this.lastTile / grid.size);
-  var cat2 = this.lastTile % grid.size;
-  var classes = ["last-tile", "cat2-" + cat2];
-  this.lastTileContainer.textContent = cat1;
-  this.applyClasses(this.lastTileContainer, classes);
+  this.refreshCategoryAssignments(grid);
 };
--- js/application.js
+++ js/application.js
@@ -1,7 +1,6 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   var grid = new GameBoard(4);
-  grid.lastTileCategoryInfo = 4;
   var act = new HtmlActuator();
   act.actuate(grid);
 });
